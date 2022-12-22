97e677bad6d1c8ce97b5db72c5c5001861752890
윤근혁 <rayman521@naver.com>
2022-12-22 18:09:00 +0900

    rayman521

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,89 @@
 <template>
-<CmmHeaderVue></CmmHeaderVue>
-<div v-if="mainDelete == false">
-<img alt="loopie" src="./assets/loopie.jpg">
-<button @click="changeDelete()"><router-link to="/list">시작하기!</router-link></button>
-</div>
-<router-view></router-view>
-<CmmFooterVue></CmmFooterVue>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>나만의 심리테스트 - 가이드</title>
+</head>
+<body>
+    <!-- 시작 페이지 -->
+    <div id="start-page">
+        <h1>나만의 심리테스트 - 가이드</h1>
+        <button @click="startpageEnd()">시작하기</button>
+        <div id="looImg">
+          <img src="./assets/loopie.jpg" alt="loopie">
+        </div>
+    </div>
+ 
+    <!-- 문제 페이지 1 -->
+    <div id="main-page1">
+        <h1>1/6</h1>
+        <h2>루피가 당신과 놀기를 원합니다. 이에 응하시겠습니까?</h2>
+        <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+        <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+        <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+        <br/>
+        <button @click="">선택</button>
+    </div>
+
+      <!-- 문제 페이지 2 -->
+      <div id="main-page2">
+        <h1>2/6</h1>
+        <h2>루피가 당신과 놀기를 원합니다. 이에 응하시겠습니까?</h2>
+        <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+        <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+        <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+        <br/>
+        <button>선택 4</button>
+    </div>
+
+      <!-- 문제 페이지 3 -->
+      <div id="main-page3">
+        <h1>3/6</h1>
+        <h2>루피가 당신과 놀기를 원합니다. 이에 응하시겠습니까?</h2>
+        <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+        <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+        <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+        <br/>
+        <button>선택 4</button>
+    </div>
+
+        <!-- 문제 페이지 4 -->
+      <div id="main-page4">
+        <h1>4/6</h1>
+        <h2>루피가 당신과 놀기를 원합니다. 이에 응하시겠습니까?</h2>
+        <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+        <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+        <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+        <br/>
+        <button>선택 4</button>
+    </div>
+
+     <!-- 문제 페이지 5 -->
+      <div id="main-page5">
+        <h1>5/6</h1>
+        <h2>루피가 당신과 놀기를 원합니다. 이에 응하시겠습니까?</h2>
+        <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+        <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+        <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+        <br/>
+        <button>선택 4</button>
+    </div>
+ 
+    <!-- 문제 페이지 5 -->
+      <div id="main-page5">
+        <h1>6/6</h1>
+        <h2>루피가 당신과 놀기를 원합니다. 이에 응하시겠습니까?</h2>
+        <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+        <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+        <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+        <br/>
+        <button>선택 4</button>
+    </div>
+    <!-- 결과 페이지 -->
+    <div id="result-page">
+        <h1>결과</h1>
+        <h2>결과 설명</h2>
+    </div>
+</body>
 </template>
 
 <script lang="ts">
@@ -17,24 +95,34 @@ import CmmFooterVue from './components/common/CmmFooter.vue';
 
 @Options({
   components: {
-    CmmHeaderVue,
-    CmmFooterVue
+    CmmHeaderVue, // 헤더 뷰 
+    CmmFooterVue  // 풋터 뷰 
   },data() {
     return {
-      mainDelete : false
+      mainDelete : false,
+      checkedValues:[] //선택된 값 세팅
     }
   },methods :{
-    changeDelete(){
-      this.mainDelete = true;
+    //체크박스 중복체크
+    clickFunc(event : any) {
+       for(let i=0; i<this.checkedValues.length;i++){
+          if(this.checkedValues[i] !== event.target.value){
+              this.checkedValues.splice(i,1);
+            }
+          }
+        },
+    //시작버튼
+    startpageEnd(){
+      let stPage = document.getElementById("start-page") as any;
+      let miPage = document.getElementById("main-page1") as any;
+        stPage.style.display = "none";
+        miPage.style.display = "block"
+    },main1(){
+      let miPage = document.getElementById("main-page1") as any;
     }
     //바운딩 순서... window.onload 의 역활 함 
   },mounted (){
-    const crUrl = window.location.href; // 현재 url 
-      const mainUrl = 'http://localhost:8080/'; // 메인화면 url
-      
-      if(crUrl != mainUrl){
-        this.mainDelete = true;
-      } 
+   
   }
 })
 export default class App extends Vue {}
@@ -48,5 +136,48 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+    background-color: white;
+    color: pink;
+}
+ 
+#start-page {
+    text-align: center;
+    margin: 0 auto;
+}
+ 
+#main-page1 {
+    text-align: center;
+    display: none;
+}
+#main-page2 {
+    text-align: center;
+    display: none;
+}
+#main-page3 {
+    text-align: center;
+    display: none;
+}
+#main-page4 {
+    text-align: center;
+    display: none;
+}
+#main-page5 {
+    text-align: center;
+    display: none;
+}
+#main-page6 {
+    text-align: center;
+    display: none;
+}
+ 
+#result-page {
+    text-align: center;
+    display: none;
+}
+#looImg {
+  margin-top: 20px;
+  border: 2ch;
 }
 </style>
--- front/src/components/view/TheContents.vue
+++ front/src/components/view/TheContents.vue
@@ -1,26 +1,17 @@
 <template>
   <table>
-    <div id="no1">
-     <h2>1/12</h2> 
+ <div id="no1">
+    <h2>1/12</h2> 
+      <br/>
+       <h4>1. 루피가 당신에게 놀러가고 부탁한다. 나의 반응은?</h4>   
      <br/>
-    <h4>1. 루피가 당신에게 놀러가고 부탁한다. 나의 반응은?</h4>   
-    <br/>
-<form name="form1">
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="chk1" value="1" v-model="checkedValues1"  @click="setCheckBoxAsRadio(this);">
-  <label class="form-check-label" for="chk">a. 좋아 좋아!</label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="chk2" value="2" v-model="checkedValues1"  @click="setCheckBoxAsRadio(this);">
-  <label class="form-check-label" for="chk">b. 시른디....</label>
-</div>
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="chk3" value="3" v-model="checkedValues1"  @click="setCheckBoxAsRadio(this);">
-  <label class="form-check-label" for="chk">c. 도민준좀...</label>
-</div>
-</form>
-</div>
-
+  <form name="form1">
+    <input type="checkbox"  value="1" name="chk" v-model="checkedValues" @change="clickFunc" :true-value="yes" :false-value="no">1. 도망가장!
+    <input type="checkbox"  value="2"  name="chk" v-model="checkedValues" @change="clickFunc">2. 따라간닷!
+    <input type="checkbox"  value="3"  name="chk" v-model="checkedValues" @change="clickFunc">3. 도민준중....
+  </form>
+<button>다음</button>
+  </div>
 </table>
 </template>
 
@@ -32,25 +23,19 @@ import { Options, Vue } from 'vue-class-component';
     name : 'TheContents',
     data (){
       return {
-        checkedValues1:[] //선택된 값 세팅
+        checkedValues:[] //선택된 값 세팅
       }
     },
     methods: {
-      setCheckBoxAsRadio(event : Element) {
-         const checkbox1 =  document.getElementById('chk1') as HTMLInputElement;
-        // const checkbox2 =  document.getElementById('chk2') as HTMLInputElement ;
-        // const checkbox3 =  document.getElementById('chk3') as HTMLInputElement ;
-         
-         for(var i=0; i<checkbox1.length; i++){
-            if(checkbox1[i] != event){
-                checkbox1[i].checked = false;
+      //체크박스 중복체크
+      clickFunc(event : any) {
+          for(let i=0; i<this.checkedValues.length;i++){
+            if(this.checkedValues[i] !== event.target.value){
+            this.checkedValues.splice(i,1);
             }
-         }
-
-        //console.log(checkbox1.checked)
-          
-          
+          }
         },
+        
     }
     
 })
